67485826d0d365add8f891e52ed53f64543dceb5
Mohammad Abu Bakar Siddqe (Jabed) <absj4k@gmail.com>
2019-12-07 16:58:31 +0600

    index.js....github-pages-issue...

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,12 +3,21 @@ window.addEventListener('load', ()=>{
   //https://developers.google.com/web/fundamentals/primers/service-workers
   
   if (navigator.serviceWorker) {
-    navigator.serviceWorker.register('serviceWorker.js').then(function(registration) {
+    //navigator.serviceWorker.register('/calcImam/serviceWorker.js')
+    navigator.serviceWorker.register('/calcImam/serviceWorker.js', {scope: '/calcImam/'}).then(function(registration) 
+    {
       console.log('ServiceWorker registration successful with scope:',  registration.scope);
     }).catch(function(error) {
       console.log('ServiceWorker registration failed:', error);
     });
   }
+//   if (navigator.serviceWorker) {
+//     navigator.serviceWorker.register('serviceWorker.js').then(function(registration) {
+//       console.log('ServiceWorker registration successful with scope:',  registration.scope);
+//     }).catch(function(error) {
+//       console.log('ServiceWorker registration failed:', error);
+//     });
+//   }
   
   });
   
